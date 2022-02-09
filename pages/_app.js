d21e5a95bf8c51047c69cf53442548ad56b7631e
Mack Bowes <mackenziepbowes@gmail.com>
2022-02-09 09:37:15 -0800

    fixies

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head></Head>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-import Head from 'next/head'
-import { Box } from '@chakra-ui/react'
-import { SimpleGrid } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import Head from "next/head";
 import {
+  Box,
+  SimpleGrid,
+  Text,
+  Image,
+  Button,
+  ButtonGroup,
   List,
   ListItem,
   ListIcon,
   OrderedList,
   UnorderedList,
-} from '@chakra-ui/react'
-import { Link } from '@chakra-ui/react'
-import {
+  Link,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -21,75 +20,201 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react'
-
-function SwapNfts() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  return (
-    <>
-      <Link onClick={onOpen}>Open Modal</Link>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text>stuff</Text>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button sx={{backgroundColor:'#FDF500'}} mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button variant='ghost'>Secondary Action</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  )
-}
+  Heading,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Head>
         <title>TrashDAO - A Collab SZNS & Trash Artists</title>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap" rel="stylesheet" />
       </Head>
-      <Box sx={{background:'#58BFAA'}}>
+      <Box
+        sx={{
+          backgroundColor: "black",
+          minHeight: `100vh`,
+          cursor: `url('/trashmouse.png'), auto`,
+          minWidth: `100vw`,
+        }}
+      >
         <header>
           <SimpleGrid columns={2} spacing={0}>
-            <Box className="logo" height='120px' sx={{background:'#58BFAA',padding:'25px'}}>
-              <Box sx={{background: '#72F44A', transform: 'rotate(-1.68deg)', width:'178px', textAlign:'center'}}>
+            <Box className="logo" height="120px" sx={{ padding: "25px" }}>
+              <Box
+                sx={{
+                  background: "#72F44A",
+                  transform: "rotate(-1.68deg)",
+                  width: "178px",
+                  textAlign: "center",
+                }}
+              >
                 <a href="#">TrashDAO</a>
               </Box>
               <br />
-              <Text fontSize='sm' sx={{color:'#72F44A',fontWeight:'700'}}>A collab SZNS & Trash Artists</Text>
+              <Text fontSize="sm" sx={{ color: "#72F44A", fontWeight: "700" }}>
+                A collab SZNS & Trash Artists
+              </Text>
             </Box>
-            <Box height='120px' sx={{background:'#58BFAA',padding:'25px',textAlign:'right'}}>
+            <Box
+              height="120px"
+              sx={{
+                padding: "25px",
+                textAlign: "right",
+              }}
+            >
               <Box>
                 <UnorderedList>
-                  <Link href="#" sx={{width:'86px', height:'26.57px', background:'#72F44A',transform: 'rotate(0.64deg)',margin:'5px'}}><ListItem sx={{display:'inline-block',fontSize:'16px',textTransform:'uppercase',fontWeight:'700',padding:'5px'}}>Swap NFT</ListItem></Link>
+                  <Link
+                    href="#"
+                    sx={{
+                      width: "86px",
+                      height: "26.57px",
+                      background: "#72F44A",
+                      transform: "rotate(0.64deg)",
+                      margin: "5px",
+                    }}
+                  >
+                    <ListItem
+                      sx={{
+                        display: "inline-block",
+                        fontSize: "16px",
+                        textTransform: "uppercase",
+                        fontWeight: "700",
+                        padding: "5px",
+                      }}
+                      onClick={onOpen}
+                    >
+                      Swap NFT
+                    </ListItem>
+                  </Link>
 
-                  <Link href="#" sx={{width:'86px', height:'26.57px', background:'#72F44A',transform: 'rotate(0.64deg)',margin:'5px'}}><ListItem sx={{display:'inline-block',fontSize:'16px',textTransform:'uppercase',fontWeight:'700',padding:'5px'}}>About</ListItem></Link>
+                  <Link
+                    href="#"
+                    sx={{
+                      width: "86px",
+                      height: "26.57px",
+                      background: "#72F44A",
+                      transform: "rotate(0.64deg)",
+                      margin: "5px",
+                    }}
+                  >
+                    <ListItem
+                      sx={{
+                        display: "inline-block",
+                        fontSize: "16px",
+                        textTransform: "uppercase",
+                        fontWeight: "700",
+                        padding: "5px",
+                      }}
+                    >
+                      About
+                    </ListItem>
+                  </Link>
                 </UnorderedList>
               </Box>
               <Box>
-                <UnorderedList sx={{float:'right'}}>
-                    <Link href="https://discord.gg/ySPNt97G7P" target="_blank"><ListItem sx={{display:'inline-block',margin:'5px'}}><Image src="/discord.png" alt="Discord" width="28.55" height="21.76" sx={{margin:'0 auto',display:'block'}} /></ListItem></Link>
-                    <Link href="https://twitter.com/Trash_DAO" target="_blank"><ListItem sx={{display:'inline-block',margin:'5px'}}><Image src="/twitter.png" alt="Twitter" width="25.5" height="20.55" sx={{margin:'0 auto',display:'block'}} /></ListItem></Link>
-                  </UnorderedList>
+                <UnorderedList sx={{ float: "right" }}>
+                  <Link href="https://discord.gg/ySPNt97G7P" target="_blank">
+                    <ListItem sx={{ display: "inline-block", margin: "5px" }}>
+                      <Image
+                        src="/discord.png"
+                        alt="Discord"
+                        width="28.55"
+                        height="21.76"
+                        sx={{ margin: "0 auto", display: "block" }}
+                      />
+                    </ListItem>
+                  </Link>
+                  <Link href="https://twitter.com/Trash_DAO" target="_blank">
+                    <ListItem sx={{ display: "inline-block", margin: "5px" }}>
+                      <Image
+                        src="/twitter.png"
+                        alt="Twitter"
+                        width="25.5"
+                        height="20.55"
+                        sx={{ margin: "0 auto", display: "block" }}
+                      />
+                    </ListItem>
+                  </Link>
+                </UnorderedList>
               </Box>
             </Box>
           </SimpleGrid>
         </header>
-        <Image src="/trashbag.png" alt="Trash Bag" width="470" height="596" sx={{margin:'0 auto',display:'block'}} />
+        <Image
+          src="/Trash.gif"
+          alt="Trash Bag"
+          width="470"
+          height="596"
+          sx={{ margin: "0 auto", display: "block" }}
+        />
       </Box>
-
-      <SwapNfts />
-      
+      <SwapNFTModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
     </>
-  )
+  );
 }
 
+function SwapNFTModal({ isOpen, onOpen, onClose }) {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent
+          sx={{
+            backgroundColor: `#E45050`,
+            border: `2px solid white`,
+            color: `white`,
+            width: `clamp(100px, 50vw, 1200px)`,
+            maxWidth: `100vw`,
+          }}
+        >
+          <ModalHeader
+            sx={{
+              color: `#FFEE04`,
+              textShadow: `0px 4px 4px rgba(0,0,0,0.3)`,
+              fontSize: `4vmax`,
+              textAlign: `center`,
+              fontFamily: `'Epilogue', sans-serif`,
+            }}
+          >
+            SWAP NFT!
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              Trash DAO is an experiment in decentralized governance and art.
+              Continue to learn more, join the community (link discord trash
+              category), and trade your NFTs for $TRASH.
+            </Text>
+            <Heading>Smart Contract</Heading>
+            <Text></Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              sx={{ backgroundColor: "#FDF500" }}
+              mr={3}
+              onClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+function TrashMouse({ x, y }) {
+  return (
+    <Image
+      src="/trashmouse.png"
+      sx={{ position: `absolute`, left: `${x}`, top: `${y}` }}
+      alt={""}
+    />
+  );
+}
